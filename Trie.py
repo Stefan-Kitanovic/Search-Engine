058ac4ef084ca7e7f02a325687a8e1de9f4ae039
gojko1001 <57567042+gojko1001@users.noreply.github.com>
2020-02-19 19:35:39 +0100

    #trie Implementacija izgradnje stabla

--- Trie.py
+++ Trie.py
@@ -1,7 +1,9 @@
+import os
+
 
 class TrieNode:
-    def __init__(self, char):
-        self.val = char
+    def __init__(self, char: str):
+        self.val = char.lower()
         self.children = dict()
         self.isWord = False
 
@@ -10,23 +12,35 @@ class Trie:
     def __init__(self):
         self.root = TrieNode('_root')
 
-    def insert(self, word):
+    def insert(self, word: str):
+        wordL = word.lower()
         curr = self.root
-        letter = ''
-        for letter in word:
+        for letter in word.lower():
             if letter not in curr.children.keys():
                 curr.children[letter] = TrieNode(letter)
             curr = curr.children[letter]
         curr.isWord = True
 
-    def search(self, word):
+    def search(self, word: str):
+        wordL = word.lower()
         curr = self.root
-        for letter in word:
+        for letter in wordL:
             if letter not in curr.children.keys():
                 return False
+            if letter == word[-1]:
+                break
             curr = curr.children[letter]
-        if curr.isWord:
-            return True
+        return curr.isWord
 
 
-
+def trieHTML(path, parser):
+    tree = Trie()
+    for root, dirs, files in os.walk(path):
+        for file in files:           # iteracija ide kroz fajlove putanje
+            currPath = os.path.join(path, file)
+            if os.path.isfile(currPath) and file.endswith('.html'):
+                p = parser.parse(currPath)
+                for word in p[1]:
+                    if not tree.search(word):
+                        tree.insert(word)
+    return tree
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import parser
-from Trie import Trie
+import os
+
+from Trie import Trie, trieHTML
+from htmlParser import Parser
+import time
+
 
 def main():
+    parser = Parser()
+    parsiraniFajlovi = list()
+    trieTree = None
     opt = -1
     while opt:
         print("Izabrati opciju: ")
@@ -13,7 +20,16 @@ def main():
         opt = input('>> ')
 
         if opt == "1":
-            pass
+            path = inputDirectory()
+            if not os.path.exists(path):
+                print("\nUneti direktorijum ne postoji!\n")
+                continue
+
+            t1 = time.time()
+            trieTree = trieHTML(path, parser)
+            t2 = time.time()
+            print('Vreme izgradnje trie stabla: ' + str(t2 - t1))
+
         elif opt == "2":
             pass
         elif opt == "3":
@@ -22,6 +38,21 @@ def main():
             pass
         elif opt == "0":
             return
+
+
+def inputDirectory():
+    if input("Nadovezati se na " + os.getcwd() + " (y/n)? ") == 'y':
+        path = os.path.abspath(os.getcwd())
+        print('-' * 10 + "Trenutni direktorijum:" + '-' * 11)
+        print(os.listdir(path))
+        print('-' * 45)
+        root = input("Uneti root direktorijum za parsiranje ('.' za trenutni): ")
+        if root != '.':
+            path = os.path.join(path, root)
+    else:
+        path = input("Uneti root direktorijum za parsiranje: ")
+
+    return path
 
 
 if __name__ == '__main__':
