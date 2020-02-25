1697fc017241ec67ed512ff6248fcfa53fa6398e
Stefan Kitanovic <33233948+StefkeTheSerb@users.noreply.github.com>
2020-02-25 21:25:24 +0100

    #rangirana_pretraga Doradjeno

--- ranking.py
+++ ranking.py
@@ -1,18 +1,18 @@
-def pageRank(filesForSorting, wordCountDict, graph):
-    rankedResults = {}
+def pageRank(graph, pagesToRank, wordCountDict):
+    rankedPages = {}
     damping = 0.85
 
     #Initial ranking (1/N)
     for page in graph.vertices():
-        rankedResults[page] = 1/len(graph.vertices())
+        rankedPages[page] = 1/len(graph.vertices())
 
     avg_change = 1
     while avg_change > 0.00001:
         changes = []
-        prevIterationResults = rankedResults
+        prevIterationResults = rankedPages
 
-        for page in rankedResults.keys():
-            startingRank = rankedResults[page]
+        for page in rankedPages.keys():
+            startingRank = rankedPages[page]
             result = 0
 
             for key in graph.vertices():
@@ -20,15 +20,28 @@ def pageRank(filesForSorting, wordCountDict, graph):
                     if page == link:
                         result += prevIterationResults[key] / len(graph.graph[key])
 
-            rankedResults[page] = (1-damping) + damping * result
-            endingRank = rankedResults[page]
+            rankedPages[page] = (1-damping)/len(rankedPages.keys()) + damping * result
+            endingRank = rankedPages[page]
             changes.append(endingRank - startingRank)
 
         avg_change = sum(changes) / len(changes) #Average change of ranking per iteration
 
-    for page in rankedResults.keys():
-        rankedResults[page] /= len(rankedResults.keys())
+    print(str(sum(rankedPages.values())))
 
+    rankedResults = {}
+    for page in pagesToRank.elems:
+        rankedResults[page] = rankedPages[page]
+
+    for page in rankedResults.keys():
+        for elem in wordCountDict:
+            rankedResults[page] *= elem.get(page, 0.5)
+
+    # for page in rankedPages.keys():
+    #     for key in graph.vertices():
+    #         for link in graph.graph[key]:
+    #             if page == link:
+    #                 for elem in wordCountDict:
+    #                     rankedResults[page] *= elem.get(page, 0.5) * 0.3
 
     sortedList = list(rankedResults.keys())
 
